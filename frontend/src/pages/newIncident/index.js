8e6d350c02c1a65983a77375433b3adb09f9a0be
Rodrigo Conte <rodrigo.conte14@gmail.com>
2020-03-28 20:27:51 -0400

    Feature: Include front end of project

---
+++ frontend/src/pages/newIncident/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FiArrowLeft } from 'react-icons/fi'
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+import { Link } from 'react-router-dom';
+
+export default function NewIncident(){
+    return (
+        <div className="new_incident">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Be the Hero"/>
+                    <h1>Cadastrar novo caso</h1>
+                    <p>Descreva o caso detalhadamente para encontrar um herói para resolver isso.</p>
+                    <Link className="back-link" to="/profile">
+                        <FiArrowLeft size={16} color="#e02041" />
+                        Voltar para home
+                    </Link>
+                </section>
+                <form>
+                    <input placeholder="Titulo do caso"/>
+                    <textarea placeholder="Descrição"/>
+                    <input placeholder="Valor em reais"/>
+
+                    <button className="button" type="submit">
+                        Cadastrar
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
